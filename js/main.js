75c785fec0ad5e69a204ee9e98f8d57408a20eb8
Daniel Schreurs <DanielSchreurs@users.noreply.github.com>
2025-11-19 11:36:25 +0100

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 const students = [
-    'Alyssa',
-    'Chiara',
-    'Isabella',
-    'Jennifer',
-    'Wassila',
-    'Assia'
+    {last_name: 'Baaroun', first_name: 'Alyssa'},
+    {last_name: 'Bibbiano', first_name: 'Chiara'},
+    {last_name: 'de Freitas', first_name: 'Isabella'},
+    {last_name: 'Dida', first_name: 'Amina'},
+    {last_name: 'Keza', first_name: 'Jennifer'},
+    {last_name: 'Meziani', first_name: 'Wassila'},
+    {last_name: 'Rabhioui', first_name: 'Assia'},
+    {last_name: 'Schmitz', first_name: 'Eline'},
+    {last_name: 'Onysiak', first_name: 'Aleksander'},
+    {last_name: 'Cakir', first_name: 'Sükeyne-Havin'},
+    {last_name: 'Jacquemin - Fanti', first_name: 'Marie'}
+
 ];
 
