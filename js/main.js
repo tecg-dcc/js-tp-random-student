48638df8164b804401038c70e51bd49522d91869
Daniel Schreurs <DanielSchreurs@users.noreply.github.com>
2025-11-19 11:11:01 +0100

    Initial commit

--- js/main.js
+++ js/main.js
@@ -6,35 +6,4 @@ const students = [
     'Wassila',
     'Assia'
 ];
-let currentIDX = 0;
 
-function shuffleStudents() {
-    for (let i = 0; i < students.length; i++) {
-        const randomIDX = Math.floor(Math.random() * students.length);
-
-        [
-            students[randomIDX],
-            students[i]
-        ] = [
-            students[i],
-            students[randomIDX]
-        ];
-    }
-}
-
-shuffleStudents();
-
-const pElement = document.createElement('p');
-
-document.body.insertAdjacentElement('beforeend', pElement);
-
-document.querySelector('form').addEventListener('submit', (evt) => {
-
-    evt.preventDefault();
-    if (currentIDX === students.length) {
-        shuffleStudents();
-        currentIDX = 0;
-    }
-    pElement.textContent = students[currentIDX];
-    currentIDX++;
-});
